e981498501633f58e9ebd669f714b5ca06e7f832
Jamen Marz <jamenmarz@gmail.com>
2017-11-30 19:27:16 -0700

    Change JS and expand WAV decoder

--- src/index.js
+++ src/index.js
@@ -2,58 +2,68 @@
 import AudioBuffer from 'audio-buffer'
 
 export default mod => {
-  const memory = new WebAssembly.Memory({ initial: 256 })
-  const inputBuffer = new Uint8Array(memory.buffer)
-  const CHUNK_SIZE = 0xFFFF * 4
+  let memory = new WebAssembly.Memory({ initial: 256 })
   let numberOfChannels = null
   let sampleRate = null
 
   return WebAssembly.instantiate(mod, {
     env: {
+      _debug (i) { console.log(i) },
       _set_params (a, b) {
+        console.log('got params', a, b)
         numberOfChannels = a
         sampleRate = b
       },
-      'g$_CHUNK_SIZE': () => CHUNK_SIZE,
       memory,
       memoryBase: 0,
       table: new WebAssembly.Table({ initial: 10, element: 'anyfunc' }),
       tableBase: 0,
-      abortStackOverflow () { throw new Error('abortStackOverflow') }
+      abortStackOverflow () { throw new Error('aborted') },
     },
     'global': { 'NaN': NaN, 'Infinity': Infinity },
     'global.Math': Math
   }).then(instance => {
-    instance.exports.__post_instantiate()
+    const { _open, _process, __post_instantiate } = instance.exports
+
+    __post_instantiate()
 
     // Open context
-    const input = 0xFFFF * 2
-    const output = 0xFFFF * 6
-    const context = instance.exports._open(input, output)
+    const inputPtr = 0xFFFF * 2
+    const outputPtr = 0xFFFF * 6
+    const maximum = 0xFFFF * 4
+    const context = _open(inputPtr, outputPtr, maximum)
 
-    function decodeChunk (chunk) {
-      // Process input
-      inputBuffer.set(chunk, input)
-      instance.exports._process(context, chunk.length)
-      // Process output
-      let length = ~~(CHUNK_SIZE / numberOfChannels)
-      let buf = new AudioBuffer({ length, numberOfChannels, sampleRate })
-      for (let c = 0; c < numberOfChannels; c++) {
-        buf.copyToChannel(new Float32Array(memory.buffer, c * length, length), c)
-      }
-      return buf
-    }
+    const input = new Uint8Array(memory.buffer, inputPtr)
+    const output = new Float32Array(memory.buffer, outputPtr + (outputPtr % 4))
 
     return function decode (chunk, cb) {
-      if (chunk === null) {
-        numberOfChannels = null
-        sampleRate = null
-        return
-      } else if (chunk.byteLength > CHUNK_SIZE) {
-        cb(null, decodeChunk(new Uint8Array(chunk, 0, CHUNK_SIZE)))
-        decode(chunk.slice(CHUNK_SIZE + 1), cb)
+      let remainder = null
+
+      if (chunk.byteLength > maximum) {
+        remainder = chunk.slice(maximum + 1)
+        chunk = new Uint8Array(chunk, 0, maximum)
       } else {
-        cb(null, decodeChunk(new Uint8Array(chunk)))
+        chunk = new Uint8Array(chunk)
+      }
+
+      input.set(chunk)
+      _process(context, 0, chunk.length)
+
+      let buf = new AudioBuffer({
+        length: chunk.length,
+        numberOfChannels,
+        sampleRate
+      })
+
+      const block = (chunk.length / numberOfChannels) / 4
+      for (let channel = 0; channel < numberOfChannels; channel++) {
+        buf.copyToChannel(output.subarray(channel * block, block), channel)
+      }
+
+      cb(null, buf)
+
+      if (remainder) {
+        return decode(remainder, cb)
       }
     }
   })
--- src/wav.c
+++ src/wav.c
@@ -2,75 +2,107 @@
 #include <stdint.h>
 #include <endian.h>
 
-#define read_int32(i,o) i[o]|(i[o+1]<<8)|(i[o+2]<<16)|(i[o+3]<<24)
-#define read_int16(i,o) i[o]|(i[o+1]<<8)
+#define util_read_int32(i,o) (i[o]|(i[o+1]<<8)|(i[o+2]<<16)|(i[o+3]<<24))
+#define util_read_int16(i,o) (i[o]|(i[o+1]<<8))
+
+#define WAVE_FORMAT_PCM 0x0001
+#define WAVE_FORMAT_FLOAT 0x0003
+#define WAVE_FORMAT_ALAW 0x0006
+#define WAVE_FORMAT_MULAW 0x0007
+#define WAVE_FORMAT_EXT 0xFFFE
+
+#define lossy_32_24_float 1.0f / (32768.0f * 65536.0f)
+
+extern void debug(int);
+extern unsigned char set_params(int, int);
 
 typedef struct {
   unsigned char* input;
   float* output;
-  uint16_t number_of_channels;
-  uint32_t sample_rate;
-  uint16_t format;
-  uint32_t avg_byte_rate;
-  uint16_t bits_per_sample;
-  unsigned char params;
+  int maximum;
+  int format;
+  int number_of_channels;
+  int sample_rate;
+  int bits_per_sample;
+  int block_align;
+  unsigned char did_header;
 } Context;
 
-extern const int CHUNK_SIZE;
-extern unsigned char set_params(uint16_t, uint32_t);
+Context* open(unsigned char* input, float* output, int length);
+void process(Context* context, int offset, const int length);
+void process_pcm(Context* context, int* offset, const int length);
+void process_float(Context* context, int* offset, const int length);
+void process_alaw(Context* context, int* offset, const int length);
+void process_mulaw(Context* context, int* offset, const int length);
+void process_ext(Context* context, int* offset, const int length);
 
-Context* open(unsigned char* input, float* output) {
-  Context context = {
-    .input = input,
-    .output = output,
-    .number_of_channels = 0,
-    .sample_rate = 0,
-    .avg_byte_rate = 0,
-    .bits_per_sample = 0,
-    .params = 0
-  };
-
-  return &context;
+Context* open(unsigned char* input, float* output, int maximum) {
+  Context* context;
+  context->input = input;
+  context->output = output;
+  context->maximum = maximum;
+  context->number_of_channels = 0;
+  context->sample_rate = 0;
+  context->block_align = 0;
+  context->bits_per_sample = 0;
+  context->did_header = 0;
+  return context;
 }
 
-void process(Context* context, int amount) {
+void process(Context* context, int offset, const int length) {
   unsigned char* input = context->input;
-  int input_step = 0;
 
-  if (context->params == 0) {
-    context->format = read_int16(input, 20);
-    context->number_of_channels = read_int16(input, 22);
-    context->sample_rate = read_int32(input, 24);
-    context->avg_byte_rate = read_int32(input, 28);
-    context->bits_per_sample = read_int16(input, 32);
+  // Process header
+  if (context->did_header == 0) {
+    context->format = util_read_int16(input, 20);
+    context->number_of_channels = util_read_int16(input, 22);
+    context->sample_rate = util_read_int32(input, 24);
+    context->block_align = util_read_int16(input, 32);
+    context->bits_per_sample = util_read_int16(input, 34);
     set_params(context->number_of_channels, context->sample_rate);
-    context->params = 1;
-    input_step = 44;
+    context->did_header = 1;
+    offset = 44;
   }
 
+  // Process contents
+  switch (context->format) {
+      case WAVE_FORMAT_PCM: process_pcm(context, &offset, length); break;
+      case WAVE_FORMAT_FLOAT: process_float(context, &offset, length); break;
+      case WAVE_FORMAT_ALAW: process_alaw(context, &offset, length); break;
+      case WAVE_FORMAT_MULAW: process_mulaw(context, &offset, length); break;
+      case WAVE_FORMAT_EXT: process_ext(context, &offset, length); break;
+  }
+}
+
+void process_pcm(Context* context, int* offset, const int length) {
+  unsigned char* input = context->input;
   float* output = context->output;
-  int number_of_channels = context->number_of_channels;
-  int bits_per_sample = context->bits_per_sample;
-  int length = CHUNK_SIZE / number_of_channels;
-  int step = 0;
+  int const bits_per_sample = context->bits_per_sample;
+  int const block = context->maximum / context->number_of_channels;
+  int index = 0;
 
-  while (input_step < amount) {
-    for (int channel = 0; channel < number_of_channels; channel++) {
-      int input_index = input_step + (channel * bits_per_sample);
-      int output_index =  step + (channel * length);
-      switch (bits_per_sample) {
-        case 8:
-          output[output_index] = ((float) input[input_index]) / 256.00f;
-          break;
-        case 16:
-          output[output_index] = ((float) input[input_index]) / 65536.00f;
-          break;
-        case 32:
-          output[output_index] = ((float) input[input_index]) / 4294967296.00f;
-          break;
+  while (*offset < length) {
+    for (int channel = 0; channel < context->number_of_channels; channel++) {
+      switch (context->bits_per_sample) {
+        case 8: output[(channel * block) + index] = ((float) input[*offset]) / 256.0f; break;
+        case 16: output[(channel * block) + index] = ((float) util_read_int16(input, *offset)) / 65536.0f; break;
+        case 32: output[(channel * block) + index] = (float) (util_read_int32(input, *offset) * lossy_32_24_float); break;
       }
-      step += 1;
-      input_step += 1;
     }
+
+    *offset += context->block_align;
+    index += 1;
   }
+}
+
+void process_float(Context* context, int* offset, const int length) {
+}
+
+void process_alaw(Context* context, int* offset, const int length) {
+}
+
+void process_mulaw(Context* context, int* offset, const int length) {
+}
+
+void process_ext(Context* context, int* offset, const int length) {
 }
