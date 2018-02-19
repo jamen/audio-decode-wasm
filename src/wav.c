f0294a63283d08cccefe76aa1c83ebcdaeaadf10
Jamen Marz <jamenmarz@gmail.com>
2018-02-19 02:11:21 -0700

    Add some misc changes

--- src/wav.c
+++ src/wav.c
@@ -2,25 +2,13 @@
 #include <stdint.h>
 #include <endian.h>
 
-#define WAVE_FORMAT_PCM 0x0001
-#define WAVE_FORMAT_FLOAT 0x0003
-#define WAVE_FORMAT_ALAW 0x0006
-#define WAVE_FORMAT_MULAW 0x0007
-#define WAVE_FORMAT_EXT 0xFFFE
-
 #define float_u8 256.0
 #define float_s16 32768.0
 #define float_s32 214748364.0
-
-#define read_int32(i,o) (i[o]|(i[o+1]<<8)|(i[o+2]<<16)|(i[o+3]<<24))
-#define read_int16(i,o) (i[o]|(i[o+1]<<8))
-#define read_uint32(i,o) (read_int16(i,o)-float_s16)
-#define read_uint16(i,o) (read_int16(i,o)-float_s16)
-
-
-extern void debug(int, double);
-extern int processing();
-extern unsigned char set_params(int, int);
+#define read_uint32(i,o) (i[o]|(i[o+1]<<8)|(i[o+2]<<16)|(i[o+3]<<24))
+#define read_uint16(i,o) (i[o]|(i[o+1]<<8))
+#define read_sint32(i,o) (read_uint16(i,o)-float_s16)
+#define read_sint16(i,o) (read_uint16(i,o)-float_s16)
 
 struct Decoder {
   unsigned char* input;
@@ -33,6 +21,9 @@ struct Decoder {
   int block_align;
   unsigned char did_header;
 };
+
+extern int _read(int);
+extern unsigned char set_params(int, int);
 
 struct Decoder* open(unsigned char* input, double* output, int maximum);
 void process(struct Decoder* decoder, int offset, int length);
@@ -60,11 +51,11 @@ void process(struct Decoder* decoder, int offset, int length) {
 
   // Process header
   if (decoder->did_header == 0) {
-    decoder->format = read_int16(input, 20);
-    decoder->number_of_channels = read_int16(input, 22);
-    decoder->sample_rate = read_int32(input, 24);
-    decoder->block_align = read_int16(input, 32);
-    decoder->bits_per_sample = read_int16(input, 34);
+    decoder->format = read_uint16(input, 20);
+    decoder->number_of_channels = read_uint16(input, 22);
+    decoder->sample_rate = read_uint32(input, 24);
+    decoder->block_align = read_uint16(input, 32);
+    decoder->bits_per_sample = read_uint16(input, 34);
     set_params(decoder->number_of_channels, decoder->sample_rate);
     decoder->did_header = 1;
     offset = 44;
@@ -72,11 +63,11 @@ void process(struct Decoder* decoder, int offset, int length) {
 
   // Process contents
   switch (decoder->format) {
-      case WAVE_FORMAT_PCM: process_pcm(decoder, offset, length); break;
-      case WAVE_FORMAT_FLOAT: process_float(decoder, offset, length); break;
-      case WAVE_FORMAT_ALAW: process_alaw(decoder, offset, length); break;
-      case WAVE_FORMAT_MULAW: process_mulaw(decoder, offset, length); break;
-      case WAVE_FORMAT_EXT: process_ext(decoder, offset, length); break;
+    case 0x0001: process_pcm(decoder, offset, length); break;
+    case 0x0003: process_float(decoder, offset, length); break;
+    case 0x0006: process_alaw(decoder, offset, length); break;
+    case 0x0007: process_mulaw(decoder, offset, length); break;
+    case 0xFFFE: process_ext(decoder, offset, length); break;
   }
 }
 
@@ -91,14 +82,15 @@ void process_pcm(struct Decoder* decoder, int offset, int length) {
   int block_align = decoder->block_align;
   int index = 0;
 
-  while (index < maximum && offset < length) {
+  while (_read()) {
+  // while (index < maximum && offset < length) {
     for (int channel = 0; channel < number_of_channels; channel++) {
       double sample;
 
       switch (bits_per_sample) {
         case 8: sample = ((double) input[offset]) / float_u8; break;
-        case 16: sample = ((double) read_uint16(input, offset)) / float_s16; break;
-        case 32: sample = ((double) read_uint32(input, offset)) / float_s32; break;
+        case 16: sample = ((double) read_sint16(input, offset)) / float_s16; break;
+        case 32: sample = ((double) read_sint32(input, offset)) / float_s32; break;
       }
 
       output[channel * section + index] = sample;
